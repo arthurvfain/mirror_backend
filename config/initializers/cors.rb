1cbda6f21ebbe2120d652c88b5f3ae5d2e0b9b87
Arthur V Fain <arthurvfain@gmail.com>
2021-09-29 16:49:11 -0400

    forgot to save

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins https:\/\/nostalgic-feynman-4827a1.netlify.app\/([a-z0-9])*
+    origins /https:\/\/nostalgic-feynman-4827a1.netlify.app\/([a-z0-9])*/
 
     resource '*',
       headers: :any,
