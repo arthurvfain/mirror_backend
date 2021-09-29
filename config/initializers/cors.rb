479a808907d1474e40201707dd1f10adf3904716
Arthur V Fain <arthurvfain@gmail.com>
2021-09-29 19:48:45 -0400

    added localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://nostalgic-feynman-4827a1.netlify.app'
+    origins 'https://nostalgic-feynman-4827a1.netlify.app, localhost:3000'
 
     resource '*',
       headers: :any,
