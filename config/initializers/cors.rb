e10ebd227be1ee9a169812c736cefbca3da55c79
Arthur V Fain <arthurvfain@gmail.com>
2021-10-01 13:28:13 -0400

    who ?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'set-cookie'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true,
-      exposedHeaders: ["Set-Cookie"]
+      credentials: true
+      # exposedHeaders: ["Set-Cookie"]
   end
 end
