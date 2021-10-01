23a6673145085a1f78e312e59889d9b27745fa4f
Arthur V Fain <arthurvfain@gmail.com>
2021-09-30 23:34:42 -0400

    no 201's for gets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     def show
         user = User.find_by(id: params[:id])
         if user
-            render json: user, serializer: UserDetailsSerializer, status: :created
+            render json: user, serializer: UserDetailsSerializer, status: :success
         else
             render json: {error: "User not Found"}, status: :not_found
         end
